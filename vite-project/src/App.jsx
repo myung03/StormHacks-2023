175baf7ea4017cfa6a0884ae6fd574a0864733c6
Matthew <matthewyungisworking@gmail.com>
2023-05-20 16:24:17 -0700

    fixing pdf submit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -7,7 +7,7 @@ import './App.css'
 function App() {
   const [sections, setSections] = useState([]);
   const [currentSection, setCurrentSection] = useState(0);  // Added this line
-
+  const [selectedFile, setSelectedFile] = useState(null);
   useEffect(() => {
     // Using fetch to get the data from your server/file.
     fetch('http://localhost:5173/lessoncontentgeneration/generatedlessoncontent.txt')
@@ -23,31 +23,52 @@ function App() {
     setCurrentSection(currentSection + 1);
   }
 
-  const FileUpload = (event) => {
-    const file = event.target.files[0];
-    // Do something with the selected file (e.g., send it to the server, process it, etc.)
-  }
+
+
+
+    const handleFileUpload = (event) => {
+      const file = event.target.files[0];
+      setSelectedFile(file);
+      // Do something with the selected file (e.g., send it to the server, process it, etc.)
+    };
+
+  const handleClearFile = () => {
+    setSelectedFile(null);
+  };
+
 
   return (
     <div className='main'>
       <Stack>
         <div>
-          <h1 className='font-extrabold leading-[3.25rem] text-4xl text-center'>
+          <h1 className='font-extrabold leading-[3.25rem] text-4xl text-center pb-10'>
             Tired of long and confusing slides? <br></br>
             <span className='gradient'>Create your personalized lesson today.</span>
           </h1>
-        </div>
-        <div className='flex flex-col items-center justify-around mx-auto my-0 gap-[20px]'>
-          <input type='file' accept='.pdf' onChange={FileUpload} className="ml-[15%]">
-          </input>
-          <Button className='max-w-xs' colorScheme='teal'>Submit</Button>
-          </div>
+          <div className="flex flex-col gap-10 justify-center items-center border-solid rounded p-[10rem]">
+      <label htmlFor="file-input" className="file-input-label">
+        {selectedFile ? selectedFile.name : 'Select File'}
+      </label>
+      <input
+        id="file-input"
+        type="file"
+        accept=".pdf"
+        onChange={handleFileUpload}
+        style={{ display: 'none' }}
+      />
+      {selectedFile && (
+        <button className="ml-10" onClick={handleClearFile}>
+          Clear File
+        </button>
+      )}
+    </div>
+    </div>
 
         {sections.slice(0, currentSection + 1).map((section, index) => 
           <Sec key={index} text={section} handleNextSection={handleNextSection} />)} 
       </Stack>
     </div>
-  )
+  );
 }
 
 export default App;
