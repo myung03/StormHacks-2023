99c9b04af93600a776228e9520bfebbcfb07cd10
Matthew <matthewyungisworking@gmail.com>
2023-05-20 14:12:30 -0700

    moved hero contents to app

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,28 @@
-import { Button, Stack } from '@chakra-ui/react'
-import Hero from './components/Hero';
+import { Button } from '@chakra-ui/react'
 import './App.css'
 
 function App() {
 
+  const FileUpload = () => {
+    const handleFileUpload = (event) => {
+      const file = event.target.files[0];
+      // Do something with the selected file (e.g., send it to the server, process it, etc.)
+    };
+  }
+
   return (
     <div className='main'>
-    <Hero></Hero>
+      <div>
+<h1 class='font-extrabold leading-[3.25rem] text-4xl text-center'>
+  Tired of long and confusing slides? <br></br>
+  <span className='gradient'>Create your personalized lesson today.</span>
+</h1>
+</div>
+<form>
+  <label>Submit a PDF of your lecture</label>
+<input type='file' accept='.pdf' ></input>
+<Button></Button>
+</form>  
 </div>
 
   )
