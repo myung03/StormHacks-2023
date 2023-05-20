e49321fecfdd6eca6a7658c058c8a5f1dd61691b
Matthew <matthewyungisworking@gmail.com>
2023-05-20 14:04:34 -0700

    moved assets

--- vite-project/src/components/Hero.jsx
+++ vite-project/src/components/Hero.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import { Button, Stack } from '@chakra-ui/react'
+import '../App.css';
 
 const Hero = () => {
   return (
-    <h1 class='rgba(0,0,0, 0.5)'>
-        Tired of long and confusing slides? <span>Create your personalized lesson today.</span>
+    <div>
+    <h1 class='font-extrabold leading-[3.25rem] text-4xl text-center'>
+        Tired of long and confusing slides? <br></br>
+        <span className='gradient'>Create your personalized lesson today.</span>
     </h1>
+    <form>
+        <label>Submit a PDF of your lecture</label>
+    <input type='file' accept='.pdf' ></input>
+    <Button></Button>
+    </form>
+    </div>
+    
   )
 }
 
