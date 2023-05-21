fe5fc249d4487639d7d175bea6fa98a79be5c7d9
Matthew <matthewyungisworking@gmail.com>
2023-05-20 17:28:32 -0700

    completed submit pdf section

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,5 +1,5 @@
 import { Button } from '@chakra-ui/react'
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import Sec from './components/Sec';
 import Stack from './components/Stack';
 import './App.css'
@@ -8,6 +8,7 @@ function App() {
   const [sections, setSections] = useState([]);
   const [currentSection, setCurrentSection] = useState(0);  // Added this line
   const [selectedFile, setSelectedFile] = useState(null);
+  const inputFile = useRef(null);
   useEffect(() => {
     // Using fetch to get the data from your server/file.
     fetch('http://localhost:5173/lessoncontentgeneration/generatedlessoncontent.txt')
@@ -23,16 +24,28 @@ function App() {
     setCurrentSection(currentSection + 1);
   }
 
+  const onButtonClick = () => {
+    // `current` points to the mounted file input element
+    inputFile.current.click();
+  };
 
+    const handleFileUpload = (file) => {
+    setSelectedFile(file);
+    // Do something with the selected file (e.g., send it to the server, process it, etc.)
+  };
 
+  const handleDrop = (event) => {
+    event.preventDefault();
+    const file = event.dataTransfer.files[0];
+    handleFileUpload(file);
+  };
 
-    const handleFileUpload = (event) => {
-      const file = event.target.files[0];
-      setSelectedFile(file);
-      // Do something with the selected file (e.g., send it to the server, process it, etc.)
-    };
-
-  const handleClearFile = () => {
+  const handleDragOver = (event) => {
+    event.preventDefault();
+  };
+  
+  const handleClearFile = (event) => {
+     event.preventDefault(); // Prevent the default button behavior
     setSelectedFile(null);
   };
 
@@ -45,23 +58,34 @@ function App() {
             Tired of long and confusing slides? <br></br>
             <span className='gradient'>Create your personalized lesson today.</span>
           </h1>
-          <div className="flex flex-col gap-10 justify-center items-center border-solid rounded p-[10rem]">
-      <label htmlFor="file-input" className="file-input-label">
-        {selectedFile ? selectedFile.name : 'Select File'}
-      </label>
+          </div>
+
+          <div className="file-upload">
+      <div
+        className={`file-drop-area ${selectedFile ? 'file-drop-area-active' : ''}`}
+        onDrop={handleDrop}
+        onDragOver={handleDragOver}
+        ref={inputFile}
+        onClick={onButtonClick}
+      >
+        {selectedFile ? (
+          <div>
+            <h3>{selectedFile.name}</h3>
+            <button className="clear-file-button" onClick={handleClearFile}>
+              Clear File
+            </button>
+          </div>
+        ) : (
+          <p>Drag and drop a PDF file here.</p>
+        )}
+      </div>
       <input
-        id="file-input"
         type="file"
         accept=".pdf"
-        onChange={handleFileUpload}
-        style={{ display: 'none' }}
+        ref={inputFile}
+        onChange={(event) => handleFileUpload(event.target.files[0])}
+        className='hidden p-[50px]'
       />
-      {selectedFile && (
-        <button className="ml-10" onClick={handleClearFile}>
-          Clear File
-        </button>
-      )}
-    </div>
     </div>
 
         {sections.slice(0, currentSection + 1).map((section, index) => 
